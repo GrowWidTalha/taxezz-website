eea84a9364d87b457e054cf8dc46d2892eed9436
Talha Ali <growwithtalha2@gmail.com>
2025-04-22 00:27:27 +0500

    removed unnecessary imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.config({ extends: ["next/core-web-vitals", "next/typescript"], rules: { "https://nextjs.org/docs/app/api-reference/config/eslint#disabling-rules": "off" } }),
+
 ];
 
 export default eslintConfig;
